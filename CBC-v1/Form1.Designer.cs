97983b3f42e8caa8547a46c7d8dc0cdf8781a45c
Kobus Jonker <jacobusjonker@gmail.com>
2020-07-09 17:54:53 +0200

    last

---
+++ CBC-v1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace CountryByCountryReportV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDestFileName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBrowseDest = new System.Windows.Forms.Button();
+            this.txtDestFolder = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.brnBrowseSource = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtDestFileName
+            // 
+            this.txtDestFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDestFileName.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDestFileName.ForeColor = System.Drawing.Color.Black;
+            this.txtDestFileName.Location = new System.Drawing.Point(181, 158);
+            this.txtDestFileName.Name = "txtDestFileName";
+            this.txtDestFileName.Size = new System.Drawing.Size(250, 21);
+            this.txtDestFileName.TabIndex = 19;
+            this.txtDestFileName.Text = "CBC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label4.Location = new System.Drawing.Point(21, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Destination File Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(157, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(292, 18);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Country By Country Report (V1)";
+            // 
+            // btnBrowseDest
+            // 
+            this.btnBrowseDest.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnBrowseDest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowseDest.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseDest.ForeColor = System.Drawing.Color.Black;
+            this.btnBrowseDest.Location = new System.Drawing.Point(437, 118);
+            this.btnBrowseDest.Name = "btnBrowseDest";
+            this.btnBrowseDest.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseDest.TabIndex = 16;
+            this.btnBrowseDest.Text = "Browse...";
+            this.btnBrowseDest.UseVisualStyleBackColor = false;
+            this.btnBrowseDest.Click += new System.EventHandler(this.btnBrowseDest_Click);
+            // 
+            // txtDestFolder
+            // 
+            this.txtDestFolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDestFolder.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDestFolder.ForeColor = System.Drawing.Color.Black;
+            this.txtDestFolder.Location = new System.Drawing.Point(181, 120);
+            this.txtDestFolder.Name = "txtDestFolder";
+            this.txtDestFolder.ReadOnly = true;
+            this.txtDestFolder.Size = new System.Drawing.Size(250, 21);
+            this.txtDestFolder.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(44, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Destination Folder:";
+            // 
+            // brnBrowseSource
+            // 
+            this.brnBrowseSource.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.brnBrowseSource.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.brnBrowseSource.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brnBrowseSource.ForeColor = System.Drawing.Color.Black;
+            this.brnBrowseSource.Location = new System.Drawing.Point(437, 72);
+            this.brnBrowseSource.Name = "brnBrowseSource";
+            this.brnBrowseSource.Size = new System.Drawing.Size(75, 23);
+            this.brnBrowseSource.TabIndex = 13;
+            this.brnBrowseSource.Text = "Browse...";
+            this.brnBrowseSource.UseVisualStyleBackColor = false;
+            this.brnBrowseSource.Click += new System.EventHandler(this.brnBrowseSource_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSource.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSource.ForeColor = System.Drawing.Color.Black;
+            this.txtSource.Location = new System.Drawing.Point(181, 74);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(250, 21);
+            this.txtSource.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(56, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Source .xlsx File:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerate.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(226, 207);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(118, 23);
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 259);
+            this.Controls.Add(this.txtDestFileName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBrowseDest);
+            this.Controls.Add(this.txtDestFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.brnBrowseSource);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDestFileName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBrowseDest;
+        private System.Windows.Forms.TextBox txtDestFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button brnBrowseSource;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
